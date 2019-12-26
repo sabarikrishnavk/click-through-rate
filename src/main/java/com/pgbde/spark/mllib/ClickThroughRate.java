73ce92ecec7da3779a4c6a179ab55a5018deef4e
Sabari Krishna <Sabari.Krishna@dcsg.com>
2019-12-26 09:53:17 -0500

    Sample100MB

--- src/main/java/com/pgbde/spark/mllib/ClickThroughRate.java
+++ src/main/java/com/pgbde/spark/mllib/ClickThroughRate.java
@@ -32,18 +32,18 @@ public class ClickThroughRate {
 
 // Load the  clickstreamdata.csv -- four attributes - “User ID”, “Song ID”,  “Date” and “Timestamp.”
 //5525c71b6213340569f3aa1abc225514,1533115844,_JUdlvPU,20180801
-		String path1 = inputPath+ "activity";
+		String path1 = inputPath+ "/activity/sample100mb.csv";
 
 //metadata folder -- “Song ID” to an “Artist ID.”A specific "Song ID" can be related to multiple "Artist IDs".
 //Zil3cVnY,516437
-		String path2 =inputPath+"newmetadata";
+		String path2 =inputPath+"/newmetadata";
 
 //Notification Clicks -Attributes: "Notification ID", "User ID", and "Date"
 //9681,69f1004d6c2395cf556c76498e041d5e,20180826
-		String path3 =inputPath+"notification_clicks";
+		String path3 =inputPath+"/notification_clicks";
 //Notification Artists-- "Notification ID" and "Artist ID"
 //9553,535722
-		String path4 =inputPath+"notification_actor";
+		String path4 =inputPath+"/notification_actor";
 
 
 		SparkSession session = SparkSession.builder().master("local")
